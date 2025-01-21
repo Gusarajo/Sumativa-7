bfbed299a7c5c4b6dac5dcb20b7992656bddf65a
Gusarajo <15132@portalcedi.edu.mx>
2025-01-21 08:56:08 -0600

    Solo falta css

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  Titulo = new FormControl('');
+  descripcion = new FormControl('');
+  fecha = new FormControl('');
+  tasks: any = [];
+  constructor() { }
+  buttonclick() {
+    if (this.Titulo.value && this.descripcion.value && this.fecha.value) {
+      const newTask = {
+        titulo: this.Titulo.value,
+        descripcion: this.descripcion.value,
+        fecha: this.fecha.value
+      };
+      this.tasks.push(newTask);
+      console.log('se agrego');
+      this.Titulo.setValue('');
+      this.descripcion.setValue('');
+      this.fecha.setValue('');
 
+    }
+  }
 }

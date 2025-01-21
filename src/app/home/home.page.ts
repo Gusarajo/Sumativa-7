4ccca11977242d647a0ebcba700c503eb05b7e9f
Gusarajo <15132@portalcedi.edu.mx>
2025-01-21 10:09:16 -0600

    Final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,21 +10,18 @@ import { FormControl } from '@angular/forms';
 export class HomePage {
 
   Titulo = new FormControl('');
-  descripcion = new FormControl('');
   fecha = new FormControl('');
   tasks: any = [];
   constructor() { }
   buttonclick() {
-    if (this.Titulo.value && this.descripcion.value && this.fecha.value) {
+    if (this.Titulo.value && this.fecha.value) {
       const newTask = {
         titulo: this.Titulo.value,
-        descripcion: this.descripcion.value,
         fecha: this.fecha.value
       };
       this.tasks.push(newTask);
       console.log('se agrego');
       this.Titulo.setValue('');
-      this.descripcion.setValue('');
       this.fecha.setValue('');
 
     }
